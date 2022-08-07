37e6bd6b7e99fb2d648416169c3a1b7778aa1a7e
William Correia <william@taurusites.com>
2022-08-07 11:32:07 +0530

    Some error checking in Commands and Data Parser

--- bot/cogs/admin_commands.py
+++ bot/cogs/admin_commands.py
@@ -6,17 +6,25 @@ class AdminCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name='history',description='Admin command for loading history into Excel File')
+    @commands.command(name='history', description='Admin command for loading history into Excel File based on user defined filter')
     @commands.has_permissions(administrator=True)
-    async def history(self,ctx, msg_contains):
-        if msg_contains == "":
-            return
+    async def history(self, ctx, arg):
+        if ctx.author.guild_permissions.administrator:
+            if arg == "":
+                await ctx.channel.send("Please enter a word/link to filter history for")
+                return
+            else:
+                if get_data(await ctx.channel.history().flatten(), arg):
+                    await ctx.channel.send("History loaded successfully")
+                else:
+                    await ctx.channel.send("An error occured writing history or no history found")
         else:
-            get_data(await ctx.channel.history().flatten(),msg_contains)
+            await ctx.channel.send("You need administrator permissions to run this command")
 
 
 def setup(bot):
     bot.add_cog(AdminCommands(bot))
 
+
 if __name__ == '__main__':
-  print('This file can only be run from Bot.py')
+    print('This file can only be run from Bot.py')
--- bot/cogs/helpers/message_writer.py
+++ bot/cogs/helpers/message_writer.py
@@ -1,14 +1,16 @@
+from xmlrpc.client import Boolean
 import pandas as pd
 
-def get_data(messages, what_to_find):
+def get_data(messages, what_to_find)->Boolean:
     data = pd.DataFrame(columns=['content', 'time', 'author', 'channel'])
     if not messages:
-        return
+        return False
     for msg in messages:
         if msg.content.find(what_to_find) != -1:
             data = data.append({'content': msg.content, 'time': msg.created_at,
                                 'author': msg.author, 'channel': msg.channel.name}, ignore_index=True)
     print(data)
+    return True
 
 
 if __name__ == '__main__':
